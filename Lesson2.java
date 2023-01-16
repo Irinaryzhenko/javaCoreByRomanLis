984aad3fd153234e35fa0dc02ef99c5d7ce29ebf
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-01-16 20:21:35 +0300

    lesson

---
+++ Lesson2.java
@@ -0,0 +1,62 @@
+import com.sun.source.util.SourcePositions;
+
+import java.sql.SQLOutput;
+
+public class Lesson2 {
+    public static void main(String[] args){
+        boolean bTrue = true;
+        boolean bFalse = false;
+        System.out.println(bTrue);
+        System.out.println(bFalse);
+
+
+        System.out.println(true);
+        System.out.println(false);
+
+        char a = 'A';
+
+        char a1 = 65;
+
+        System.out.println(a);
+        System.out.println(a1);
+
+        System.out.println('A');
+        System.out.println(65);
+
+        byte b = 19;
+        System.out.println(b);
+
+        short s = 5;
+        System.out.println(s);
+
+        int i = 1;
+        int i1 = 10_000_000;
+        int c = 035;
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(i1);
+
+        long l = 1_909_897_786_768_787_689l;
+        System.out.println(l);
+
+        float f = 1.35f;
+
+        System.out.println(f);
+        System.out.println(1.35);
+
+        double d = 1.34;
+        double d1 = 1.34d;
+
+        System.out.println(d);
+        System.out.println(d1);
+
+        String message = "Hello!";
+        String message1; //null
+        message1 = "Some super text";
+        System.out.println(message1);
+        System.out.println("Hello!");
+
+
+    }
+}
