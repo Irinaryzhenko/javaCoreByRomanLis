eaa635a1cd602ba6a4c900019d4c1b176f35fc6f
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-01-23 19:16:43 +0300

    tasks if else switch

---
+++ lesson4.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class lesson4 {
+    public static void main (String[] args){
+        int X = 10;
+        int Y = 2;
+        int Z = 5;
+
+        if (X > Z) {
+            System.out.println(X + Y);
+        } else {
+            System.out.println(Z);
+        }
+        System.out.println((X + Y + Z) / 3.0);
+
+    }
+}
